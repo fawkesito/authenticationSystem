78df372186f94cb3076fbe2f0fdca53fbd0f6155
fawkesito <snakemartinez10@gmail.com>
2022-07-07 18:02:14 -0500

    Finished

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 const dbConnect = require('./db/dbConnect');
 const User = require("./db/userModel");
+const auth = require("./auth");
 
 
 
@@ -19,11 +20,26 @@ app.get("/", (request, response, next) => {
 
 // execute database connection
 dbConnect();
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+
 // login endpoint
 app.post("/login", (request, response) => {
   // check if email exists
   User.findOne({ email: request.body.email })
-
     // if email exists
     .then((user) => {
       // compare the password entered and the hashed password found
@@ -115,5 +131,14 @@ app.post("/register", (request, response) => {
     });
 });
 
+// free endpoint
+app.get("/free-endpoint", (request, response) => {
+  response.json({ message: "You are free to access me anytime" });
+});
+
+// authentication endpoint
+app.get("/auth-endpoint", auth, (request, response) => {
+  response.json({ message: "You are authorized to access me" });
+});
 
  module.exports = app;
